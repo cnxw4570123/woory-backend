17c8b8d939c41795e6ab7b08324cd93786e200d2
JGM <cnxw4570123@gmail.com>
2024-07-06 12:51:19 +0900

    chore : 프로젝트 환경 구성

---
+++ src/main/java/com/woory/backend/WooryApplication.java
@@ -0,0 +1,13 @@
+package com.woory.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WooryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WooryApplication.class, args);
+	}
+
+}
