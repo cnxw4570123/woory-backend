6097fd754a26d1f2a48b2057857379f5d84bef3a
JGM <cnxw4570123@gmail.com>
2024-07-13 14:01:51 +0900

    refactor : CustomOAuth2User 클래스 수정
    기존 User 엔티티가 아닌 UserDto를 데코레이팅 하도록 수정

--- src/main/java/com/woory/backend/dto/CustomOAuth2User.java
+++ src/main/java/com/woory/backend/dto/CustomOAuth2User.java
@@ -5,47 +5,48 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-
-import com.woory.backend.entity.User;
 
 public class CustomOAuth2User implements OAuth2User {
 
-    private final User user;
+	private final UserDto user;
 
-    public CustomOAuth2User(User user) {
-        this.user = user;
-    }
+	public CustomOAuth2User(UserDto user) {
+		this.user = user;
+	}
 
-    @Override
-    public Map<String, Object> getAttributes() {
-        return Map.of();
-    }
+	@Override
+	public Map<String, Object> getAttributes() {
+		return Map.of();
+	}
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        Collection<GrantedAuthority> collection = new ArrayList<>();
-        collection.add(new GrantedAuthority() {
-            @Override
-            public String getAuthority() {
-                return user.getRole();
-            }
-        });
-        return collection;
-    }
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		Collection<GrantedAuthority> collection = new ArrayList<>();
+		collection.add(new GrantedAuthority() {
+			@Override
+			public String getAuthority() {
+				return user.getRole();
+			}
+		});
+		return collection;
+	}
 
-    @Override
-    public String getName() {
-        return user.getNickname();
-    }
-    public String getUsername(){
-        return user.getUsername();
-    }
+	@Override
+	public String getName() {
+		return user.getNickname();
+	}
 
-    public String getEmail() {
-        return user.getEmail();
-    }
+	public Long getUserId() {
+		return user.getUserId();
+	}
 
+	public String getUsername() {
+		return user.getUsername();
+	}
+
+	public String getEmail() {
+		return user.getEmail();
+	}
 
 }
