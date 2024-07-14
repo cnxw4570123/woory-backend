a2b0f163445d7ac8917b9a738759fd430c5cc7ac
JGM <cnxw4570123@gmail.com>
2024-07-14 19:22:55 +0900

    fix : 로그인 기능 수정
    - 최초 로그인 이외에는 소셜 로그인 연동이 필요하지 않기 때문에 loadUser메서드 내에서 바로 OAuth 액세스 토큰을 만료시키는 작업 추가
    - 이 과정에서 WebClient 인터페이스를 사용하기위해서 의존성 추가

--- src/main/java/com/woory/backend/service/CustomOAuth2UserService.java
+++ src/main/java/com/woory/backend/service/CustomOAuth2UserService.java
@@ -1,7 +1,5 @@
 package com.woory.backend.service;
 
-import java.util.Optional;
-
 import com.woory.backend.dto.CustomOAuth2User;
 import com.woory.backend.dto.KakaoResponse;
 import com.woory.backend.dto.NaverResponse;
@@ -12,11 +10,17 @@ import com.woory.backend.repository2.UserRepository;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -32,6 +36,8 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 	public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
 		OAuth2User oAuth2User = super.loadUser(userRequest);
 		log.info("oAuth2User = {}", oAuth2User.getAttributes());
+		String accessToken = userRequest.getAccessToken().getTokenValue();
+		ClientRegistration clientRegistration = userRequest.getClientRegistration();
 
 		String registration = userRequest.getClientRegistration().getRegistrationId();
 		OAuth2Response oAuth2Response;
@@ -42,6 +48,9 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 		} else {
 			return null;
 		}
+
+		oAuth2LogoutImmediately(accessToken, clientRegistration);
+
 		//구현
 		String username = oAuth2Response.getProvider() + " " + oAuth2Response.getProviderId();
 
@@ -60,6 +69,49 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 		byUsername.setEmail(oAuth2Response.getEmail());
 		byUsername.setProfileImage(oAuth2Response.getProfileImage());
 		return new CustomOAuth2User(UserDto.fromUser(userRepository.save(byUsername)));
+	}
+
+	private void oAuth2LogoutImmediately(String accessToken, ClientRegistration registration) {
+		WebClient webClient = WebClient.builder()
+			.build();
+
+		if(registration.getRegistrationId().equals("naver")){
+			MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+			params.add("grant_type", "delete"); // 토큰 삭제 시 delete
+			params.add("client_id", registration.getClientId());
+			params.add("client_secret", registration.getClientSecret());
+			params.add("access_token", accessToken);
+			String response = webClient.post()
+				.uri(uriBuilder -> uriBuilder.scheme("https")
+					.host("nid.naver.com")
+					.path("oauth2.0/token")
+					.build()
+				)
+				.body(BodyInserters.fromFormData(params))
+				.header("Content-type", MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+
+			log.info("response = {}", response);
+			return;
+		}
+
+		if (registration.getRegistrationId().equals("kakao")) {
+			String response = webClient.post()
+				.uri(uriBuilder -> uriBuilder.scheme("https")
+					.host("kapi.kakao.com")
+					.path("v1/user/logout")
+					.build()
+				)
+				.header("Authorization", "Bearer " + accessToken)
+				.header("Content-type", MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+				.retrieve()
+				.bodyToMono(String.class)
+				.block();
+
+			log.info("response = {}", response);
+		}
 	}
 
 }
