7b940cf7d3d64ba3b82d29ca256e493072ef70e2
JGM <cnxw4570123@gmail.com>
2024-07-14 14:22:54 +0900

    refactor : UserRepository 메서드 반환타입 수정
    - findByXXX메서드가 null-safe한 Optional로 조회해오도록 수정

--- src/main/java/com/woory/backend/repository2/UserRepository.java
+++ src/main/java/com/woory/backend/repository2/UserRepository.java
@@ -1,10 +1,12 @@
 package com.woory.backend.repository2;
 
+import java.util.Optional;
+
 import com.woory.backend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-    User findByUserId(long id);
+    Optional<User> findByUsername(String username);
+    Optional<User> findByUserId(long id);
 
 }
