9bc24d0b9764cda9e2a5c1dd31d94eeade0b3df8
JGM <cnxw4570123@gmail.com>
2024-07-19 00:25:58 +0900

    feat : TopicManager 클래스 추가
    - 일주일간 중복없는 토픽을 뽑기 위해 TopicManager 클래스 추가
    - 날짜별 토픽 번호를 나타내는 TopicByDate 클래스 추가

---
+++ src/main/java/com/woory/backend/entity/TopicManager.java
@@ -0,0 +1,91 @@
+package com.woory.backend.entity;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TopicManager {
+	private static final Random randomTopic = new Random(System.currentTimeMillis());
+	private static final Path TOPIC_PATH = Paths.get("src/main/resources/topics.txt");
+	private static final Integer TOPIC_COUNT = 80;
+	private static Deque<TopicByDate> topicOfLast7Days = init();
+
+	private TopicManager() {
+	}
+
+	private static Deque<TopicByDate> init() {
+		List<TopicByDate> topics;
+		try {
+			topics = Files.readAllLines(TOPIC_PATH).stream()
+				.filter(s -> !s.isEmpty())
+				.map(s -> {
+					try {
+						return new TopicByDate(s);
+					} catch (ParseException e) {
+						throw new RuntimeException(e);
+					}
+				})
+				.toList();
+		} catch (IOException e) {
+			topics = new ArrayList<>();
+		}
+
+		LocalDate now = LocalDate.now();
+
+		Deque<TopicByDate> topicIndex = new ArrayDeque<>(topics);
+		if (topicIndex.isEmpty() || topicIndex.peekLast().getDate().isBefore(now)) {
+			topicIndex.add(new TopicByDate(randomTopic.nextInt(TOPIC_COUNT)));
+		}
+
+		try {
+			Files.writeString(TOPIC_PATH, topicIndex.stream().map(String::valueOf).collect(Collectors.joining("\n")),
+				StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE,
+				StandardOpenOption.SYNC);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+		return topicIndex;
+	}
+
+	public static int getTopicOfToday() {
+		return topicOfLast7Days.peekLast().getTopicIndex();
+	}
+
+	public static int pollTopicOfToday() {
+		if (topicOfLast7Days.size() == 7) {
+			topicOfLast7Days.poll();
+		}
+		int topic;
+		do {
+			topic = randomTopic.nextInt(TOPIC_COUNT);
+		} while (topicOfLast7Days.contains(topic));
+
+		topicOfLast7Days.add(new TopicByDate(topic));
+		writeTopicsAsFile();
+		return topic;
+	}
+
+	private static void writeTopicsAsFile() {
+		try {
+			Files.writeString(TOPIC_PATH,
+				topicOfLast7Days.stream().map(String::valueOf).collect(Collectors.joining("\n")),
+				StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE,
+				StandardOpenOption.SYNC);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
