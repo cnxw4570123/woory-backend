424338137960c2b77d5396f745b0fdae24b72452
JGM <cnxw4570123@gmail.com>
2024-07-22 03:54:32 +0900

    fix : SecurityUtils 수정

--- src/main/java/com/woory/backend/utils/SecurityUtil.java
+++ src/main/java/com/woory/backend/utils/SecurityUtil.java
@@ -1,14 +1,21 @@
 package com.woory.backend.utils;
 
+import java.util.HashMap;
+import java.util.UUID;
+
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
 import com.woory.backend.dto.CustomOAuth2User;
 
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
+@Component
 public class SecurityUtil {
+	private static HashMap<String, CustomOAuth2User> uuidToUser = new HashMap<>();
+
 	public static Long getCurrentUserId() {
 		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
@@ -17,5 +24,13 @@ public class SecurityUtil {
 			throw new RuntimeException("인증 정보가 없습니다.");
 		}
 		return Long.valueOf(oAuth2User.getName());
+	}
+
+	public static void saveUserWithUUID(UUID uuid, CustomOAuth2User user) {
+		uuidToUser.put(uuid.toString(), user);
+	}
+
+	public static CustomOAuth2User getUserByCode(String code) {
+		return uuidToUser.get(code);
 	}
 }
