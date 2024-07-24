357afdfa0199169ebbef09c2d96f76beb19aa65e
JGM <cnxw4570123@gmail.com>
2024-07-24 21:01:42 +0900

    refactor : 리액션 조회 기능 수정
    - 리액션 타입별 개수와 본인 여부를 나타내기 위해 속성 추가

--- src/main/java/com/woory/backend/repository/ContentReactionRepository.java
+++ src/main/java/com/woory/backend/repository/ContentReactionRepository.java
@@ -4,11 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.woory.backend.entity.ContentReaction;
 
 public interface ContentReactionRepository extends JpaRepository<ContentReaction, Long> {
 	List<ContentReaction> findByContent_ContentId(Long contentId);
+
+	@Query("select cr from ContentReaction cr join fetch cr.user u where cr.content.contentId = :contentId")
+	List<ContentReaction> findByContentIdWithUser(@Param("contentId") Long contentId);
 
 	Optional<ContentReaction> findContentReactionByContent_ContentIdAndUser_UserId(Long contentId, Long userId);
 }
