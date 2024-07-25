e84761873a820852db66fe0123e9ad89f104b539
JGM <cnxw4570123@gmail.com>
2024-07-25 12:55:38 +0900

    test : 토픽 자동 생성 문제를 파악하기 위해 로그 표시를 위한 코드 추가

--- src/main/java/com/woory/backend/repository/BatchTopicRepositoryImpl.java
+++ src/main/java/com/woory/backend/repository/BatchTopicRepositoryImpl.java
@@ -4,6 +4,8 @@ import java.util.List;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSourceUtils;
@@ -14,6 +16,7 @@ import com.woory.backend.dto.TopicDto;
 @Repository
 public class BatchTopicRepositoryImpl implements BatchTopicRepository{
 
+	private static final Logger log = LoggerFactory.getLogger(BatchTopicRepositoryImpl.class);
 	private NamedParameterJdbcTemplate jdbcTemplate;
 
 	@Autowired
@@ -23,6 +26,7 @@ public class BatchTopicRepositoryImpl implements BatchTopicRepository{
 
 	@Override
 	public int[] saveAll(List<TopicDto> topics) {
+		log.info("토픽 배치 저장 실행");
 		return jdbcTemplate.batchUpdate(
 			"INSERT INTO TOPIC(topic_content, issue_date, group_id, topic_byte) VALUES(:topicContent, :issueDate, :groupId, :topicByte)",
 			SqlParameterSourceUtils.createBatch(topics)
