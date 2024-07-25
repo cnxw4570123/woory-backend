47a6301db7bcfa9121b3d8108b7b7c62c47af9fe
JGM <cnxw4570123@gmail.com>
2024-07-26 02:14:49 +0900

    feat : 그룹 추방 기능 수정
    - 기존 추방 이후 다시는 가족에 들어올 수 없었던 문제를 추방 이후에도 참여 가능하도록 수정
    - 그 과정에서 GroupStatus.BANNED를 사용하지 않도록 수정하였고 해당 속성을 사용하던 쿼리문과 ContentService 코드에서 삭제함

--- src/main/java/com/woory/backend/repository/GroupUserRepository.java
+++ src/main/java/com/woory/backend/repository/GroupUserRepository.java
@@ -15,19 +15,19 @@ import java.util.Optional;
 
 @Repository
 public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
-	@Modifying
+	@Modifying(clearAutomatically = true)
 	@Query("UPDATE GroupUser gu SET gu.status = :newStatus WHERE gu.group.groupId = :groupId AND gu.user.userId = :userId")
 	void updateStatusByGroup_GroupIdAndUser_UserId(@Param("groupId") Long groupId, @Param("userId") Long userId,
 		@Param("newStatus") GroupStatus newStatus);
 
-	@Query("select gu from GroupUser gu where gu.group.groupId = :groupId and gu.status != com.woory.backend.entity.GroupStatus.BANNED order by gu.regDate asc")
-	List<GroupUser> findActiveGroupUsersByGroupIdOrderByRegDate(@Param("groupId") Long groupId);
+	@Query("select gu from GroupUser gu where gu.group.groupId = :groupId order by gu.regDate asc")
+	List<GroupUser> findGroupUsersByGroupIdOrderByRegDate(@Param("groupId") Long groupId);
 
 	// 그룹 유저에서 유저 아이디로 검색 후 그룹까지 조회
-	@Query("select new com.woory.backend.dto.GroupInfoDto(g.groupId, g.groupName, g.photoPath, gu.status) from GroupUser gu join gu.group g on g = gu.group where gu.status != com.woory.backend.entity.GroupStatus.BANNED and gu.user.userId = :userId order by gu.regDate asc")
+	@Query("select new com.woory.backend.dto.GroupInfoDto(g.groupId, g.groupName, g.photoPath, gu.status) from GroupUser gu join gu.group g on g = gu.group and gu.user.userId = :userId order by gu.regDate asc")
 	List<GroupInfoDto> findMyGroupInfoDto(@Param("userId") Long userId);
 
-	@Query("select new com.woory.backend.dto.GroupInfoDto(g.groupId, g.groupName, g.photoPath, gu.status) from GroupUser gu join gu.group g on g = gu.group where gu.status != com.woory.backend.entity.GroupStatus.BANNED and gu.group.groupId = :groupId order by gu.regDate asc")
+	@Query("select new com.woory.backend.dto.GroupInfoDto(g.groupId, g.groupName, g.photoPath, gu.status) from GroupUser gu join gu.group g on g = gu.group and gu.group.groupId = :groupId order by gu.regDate asc")
 	List<GroupInfoDto> findMyGroupInfoDtoByGrooupId(@Param("groupId") Long groupId);
 
 	Optional<GroupUser> findByUser_UserIdAndGroup_GroupId(Long userId, Long groupId);
@@ -42,7 +42,7 @@ public interface GroupUserRepository extends JpaRepository<GroupUser, Long> {
 
 	boolean existsByGroup_GroupId(Long groupId);
 
-	@Query("select gu from GroupUser gu where gu.group.groupId = :groupId and gu.user.userId != :userId and gu.status != com.woory.backend.entity.GroupStatus.BANNED")
+	@Query("select gu from GroupUser gu where gu.group.groupId = :groupId and gu.user.userId != :userId")
 	List<GroupUser> findGroupUserWithoutUser(@Param("groupId") Long groupId, @Param("userId") Long userId);
 
 	@Query("select gu from GroupUser gu join fetch gu.user u where u.userId = :userId and gu.group.groupId = :groupId")
