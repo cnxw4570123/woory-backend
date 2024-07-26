f698ed9a2a8dc6d879d474d60bbdcf1db45a522e
JGM <cnxw4570123@gmail.com>
2024-07-26 19:59:13 +0900

    fix : DB에서 엔티티 삭제 이후 속성 가져오던 문제 수정

--- src/main/java/com/woory/backend/service/ContentService.java
+++ src/main/java/com/woory/backend/service/ContentService.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.apache.http.util.TextUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -85,13 +84,14 @@ public class ContentService {
 
 		Content content = contentRepository.findById(contentId)
 			.orElseThrow(() -> new CustomException(ErrorCode.CONTENT_NOT_FOUND));
+		String contentImgPath = content.getContentImgPath();
 
 		//본인의 것만 삭제하기 위해서
 		if (!content.getUsers().getUserId().equals(userId)) {
 			throw new CustomException(ErrorCode.NO_PERMISSION_TO_DELETE);
 		}
 		contentRepository.delete(content);
-		awsService.deleteImage(content.getContentImgPath());
+		awsService.deleteImage(contentImgPath);
 	}
 
 	@Transactional
