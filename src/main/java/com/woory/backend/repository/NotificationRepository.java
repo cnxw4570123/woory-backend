ece7257765505f04c98ff88da9466f059d53541e
JGM <cnxw4570123@gmail.com>
2024-08-03 10:33:23 +0900

    feat : 알림함 조회 기능 추가
    - NotificationRepository에 저장된 알림을 불러오는 메서드 추가
    - 새글이나 토픽 알림일 경우 그룹에 속한 모두에게, 반응 알림일 경우 댓글 또는 글 작성자로 조회
    - 이를 받아올 Dto 인터페이스 추가

---
+++ src/main/java/com/woory/backend/repository/NotificationRepository.java
@@ -0,0 +1,29 @@
+package com.woory.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.woory.backend.dto.NotificationDto;
+import com.woory.backend.entity.Notification;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+	@Query(
+		"select n.groupId, n.topicDate, n.topicTitle, n.contentUserId, n.contentId, n.commentUserId, n.commentId, n.replyUserId, n.replyId, n.reactionUserId, n.reactionId, n.userId, n.notificationType,"
+			+ "case when n.contentUserId is not null then (select u.username from User u where u.userId = n.contentUserId)"
+			+ "when n.commentUserId is not null then (select u.username from User u where u.userId = n.commentUserId) "
+			+ "when n.replyUserId is not null then (select u.username from User u where u.userId = n.replyUserId) "
+			+ "when n.reactionUserId is not null then (select u.username from User u where u.userId = n.reactionUserId) "
+			+ "else null "
+			+ "end as author "
+			+ "from Notification n "
+			+ "where (n.notificationType in (com.woory.backend.entity.NotificationType.CONTENT, com.woory.backend.entity.NotificationType.TOPIC) and n.groupId = :groupId) "
+			+ "or n.userId = :userId "
+			+ "order by n.issueDate desc "
+			+ "limit 10")
+	List<NotificationDto> findAllByUserId(@Param("groupId") Long groupId, @Param("userId") Long userId);
+}
