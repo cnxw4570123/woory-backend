693e5f4367b98b9b0ebaa6898fa3ffba035a9ed9
JGM <cnxw4570123@gmail.com>
2024-08-05 11:52:10 +0900

    refactor : 알림 서비스 수정
    - 본인 게시글, 댓글에 대해서는 댓글, 답글 반응 생성해도 알림을 저장하지 않도록 수정

--- src/main/java/com/woory/backend/service/NotificationService.java
+++ src/main/java/com/woory/backend/service/NotificationService.java
@@ -1,9 +1,13 @@
 package com.woory.backend.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.woory.backend.dto.NotificationDto;
 import com.woory.backend.entity.Notification;
 import com.woory.backend.repository.NotificationRepository;
+import com.woory.backend.utils.SecurityUtil;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
