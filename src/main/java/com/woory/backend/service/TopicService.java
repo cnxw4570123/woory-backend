72900273b13c39a4ee0979e6e1bc511c17c09d31
JGM <cnxw4570123@gmail.com>
2024-08-05 13:33:02 +0900

    feat : 토픽 알림 생성 기능 추가
    - 매일 10시에 토픽 알림 생성하도록 추가

--- src/main/java/com/woory/backend/service/TopicService.java
+++ src/main/java/com/woory/backend/service/TopicService.java
@@ -1,8 +1,12 @@
 package com.woory.backend.service;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 import java.util.List;
 
+import com.woory.backend.entity.Notification;
+import com.woory.backend.entity.Topic;
 import com.woory.backend.error.CustomException;
 import com.woory.backend.error.ErrorCode;
 
@@ -16,27 +20,23 @@ import com.woory.backend.dto.TopicDto;
 import com.woory.backend.entity.TopicManager;
 import com.woory.backend.entity.TopicSet;
 import com.woory.backend.repository.GroupRepository;
+import com.woory.backend.repository.NotificationRepository;
 import com.woory.backend.repository.TopicRepository;
 import com.woory.backend.repository.TopicSetRepository;
 
 import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional
+@RequiredArgsConstructor
 public class TopicService {
 
 	private static final Logger log = LoggerFactory.getLogger(TopicService.class);
 	private final TopicRepository topicRepository;
 	private final GroupRepository groupRepository;
 	private final TopicSetRepository topicSetRepository;
-
-	@Autowired
-	public TopicService(TopicRepository topicRepository, GroupRepository groupRepository,
-		TopicSetRepository topicSetRepository) {
-		this.topicRepository = topicRepository;
-		this.groupRepository = groupRepository;
-		this.topicSetRepository = topicSetRepository;
-	}
+	private final NotificationRepository notificationRepository;
 
 	@Scheduled(cron = "0 0 0 * * *", zone = "Asia/Seoul") // 매일 0시에 실행
 	public void generateTopics() {
@@ -45,7 +45,7 @@ public class TopicService {
 		long topic = TopicManager.pollTopicOfToday();
 
 		TopicSet topicSet = topicSetRepository.findTopicSetById(topic)
-				.orElseThrow(() -> new CustomException(ErrorCode.TOPIC_NOT_FOUND));
+			.orElseThrow(() -> new CustomException(ErrorCode.TOPIC_NOT_FOUND));
 		Date now = new Date();
 
 		List<TopicDto> topicDtos = groupRepository.findAll().stream()
@@ -54,5 +54,22 @@ public class TopicService {
 			).toList();
 
 		topicRepository.saveAll(topicDtos);
+	}
+
+	/**
+	 * 매일 오전 10시에 토픽알림 생성
+	 */
+	@Scheduled(cron = "0 0 10 * * *", zone = "Asia/Seoul")
+	public void saveTopicNotifications() {
+		log.info("토픽 알람 생성 시작");
+
+		Date today = new Date();
+		List<Notification> notifications = topicRepository.findAllWithGroupsByIssueDate(today)
+			.stream()
+			.map(t -> Notification.fromCreatingTopic(t.getGroup().getGroupId(), t.getTopicId(), t.getIssueDate(),
+				t.getTopicContent(), today))
+			.toList();
+
+		notificationRepository.saveAll(notifications);
 	}
 }
